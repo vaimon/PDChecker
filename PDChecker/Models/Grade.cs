b0a24b6ab5e40ab766daa8a8d6e6867c7ef67129
Andrew Nikolaev <Andrew.Vaimon@yandex.ru>
2023-06-07 14:17:17 +0300

    update connection string

--- PDChecker/Models/Grade.cs
+++ PDChecker/Models/Grade.cs
@@ -2,8 +2,12 @@
 
 public class Grade
 {
+    public int Id { get; set; }
     public int JudgeId { get; set; }
     public User Judge { get; set; }
     public int ProjectId { get; set; }
     public Project Project { get; set; }
+    public double Points { get; set; }
+    public string Feedback { get; set; }
+    
 }
